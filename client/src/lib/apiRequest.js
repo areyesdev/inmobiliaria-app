d58a656c8ce96425d0dfb75e4abacf818a13b70b
Andres Reyes <areyesdev@gmail.com>
2024-06-24 20:21:01 -0500

    add: client auth in backend

---
+++ client/src/lib/apiRequest.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+const apiRequest = axios.create({ baseURL: "http://localhost:8800/api", withCredentials: true });
+
+export default apiRequest;
--- client/src/routes/register/register.jsx
+++ client/src/routes/register/register.jsx
@@ -1,16 +1,43 @@
 import "./register.scss";
-import { Link } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import apiRequest from "../../lib/apiRequest";
 
 function Register() {
+
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handlerSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    setIsLoading(true);
+    const formData = new FormData(e.target);
+    const data = {
+      username: formData.get("username"),
+      email: formData.get("email"),
+      password: formData.get("password"),
+    };
+    try {
+      const res = await apiRequest.post("/auth/register", data);
+      navigate("/login");
+    } catch (err) {
+      setError(err.response.data.message);
+    }
+  } 
   return (
     <div className="register">
       <div className="formContainer">
-        <form>
+        <form onSubmit={handlerSubmit}>
           <h1>Create an Account</h1>
           <input name="username" type="text" placeholder="Username" />
           <input name="email" type="text" placeholder="Email" />
           <input name="password" type="password" placeholder="Password" />
-          <button >Register</button>
+          <button disabled={isLoading}>Register</button>
+          {error && <span>{error}</span>}
           <Link to="/login">Do you have an account?</Link>
         </form>
       </div>
